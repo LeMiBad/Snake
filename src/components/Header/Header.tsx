ea77dc6c71668ac4866ee236c2ea59a198673bc0
Leonid <79872121399.yandex@gmail.com>
2022-10-18 20:51:43 +0300

    animation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,25 +1,13 @@
-import { useState } from 'react'
-import { Link, Navigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import css from './Header.module.sass'
 
 const Header = () => {
-    const [url, setUrl] = useState(null) as any
-
-    const toMain = () => {
-        setTimeout(() => {
-            setUrl('/')
-        }, 1500)
-        setUrl(null)
-    }
-
-    if(url) return <Navigate to={`${url}`}/>
-
     return (
         <>
             <header>
                 <a href='https://lemibad.github.io/react-game/' target={'blank'} className={css.gameName}>Квадраты</a>
                 <h1 className={css.gameName}>Змейка</h1>
-                <button onClick={toMain} style={{cursor: 'pointer'}}>Назад</button>
+                <Link style={{cursor: 'pointer'}} to={'/'}>Назад</Link>
                 <Link style={{cursor: 'pointer'}} to={'/settings'}>Настройки</Link>
             </header>
         </>
